435a9cf81fae46106d3576d69318b0b522887b0b
Maria Castro <mariacastro87@gmail.com>
2018-01-27 15:57:54 -0300

    Test mouse input

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -5,46 +5,25 @@ import 'pixi'
 import 'p2'
 import 'phaser'
 
-const logoImage = require('assets/phaser.png')
+import { MainState } from './states/main'
 
-class SimpleGame {
-    game: Phaser.Game;    
-    constructor() {
-        const config: Phaser.IGameConfig = {
-            width:           800, 
-            height:          600,
-            renderer:        Phaser.AUTO,   
-            parent:          'content',
-            resolution:      1,
-            forceSetTimeOut: false,
-            state: { preload: this.preload, create: this.create }
-        }
-        this.game = new Phaser.Game(config)
+export default class App extends Phaser.Game {
+    constructor(config: Phaser.IGameConfig) {
+      super(config)
+      //this.state.add('intro', IntroState)      
+      this.state.add('main', MainState)
+      this.state.start('main')
     }
-
-
-    preload() {
-        this.game.load.image('logo', logoImage)
-    }
-
-    create() {
-        const logo: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo')
-
-        const textStyle: Phaser.PhaserTextStyle = {
-            font: "15px Arial", 
-            fill: "#19de65" ,
-            align:  'left',
-            boundsAlignH: 'center',
-            boundsAlignV: 'bottom'
-        }
-        const text: Phaser.Text = this.game.add.text(0, 0, 'Hello World!', textStyle)
-        
-        text.setTextBounds(16, 16, 768, 568)
-        logo.anchor.setTo(0.5, 0.5)
-    }
-
-}
+  }
 
 window.onload = () => {
-    const game = new SimpleGame()
-};
+    const config: Phaser.IGameConfig = {
+      width:           770, 
+      height:          600,
+      renderer:        Phaser.AUTO,       //Reseach further about Phaser.WEBGL_MULTI multi-texture rendering
+      parent:          'content',
+      resolution:      1,
+      forceSetTimeOut: false
+    }
+    new App(config)
+}
